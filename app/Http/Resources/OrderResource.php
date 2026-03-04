62507a9657de3c5ac1c9458b996fd576ae9c64bb
milanmarko <marko.milan@students.jedlik.eu>
2026-03-04 14:37:26 +0100

    Refactor AuthController and OrderController; add OrderResource and OrderItemResource

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'order_identifier_number' => $this->order_identifier_number,
+            'user_id' => $this->user_id,
+            'status' => $this->status->name,
+            'delivery_date' => $this->delivery_date,
+            'items' => OrderItemResource::collection($this->items),
+            'total_price' => $this->items->sum(fn($item) => $item->pivot->quantity * $item->price),
+            'default_completion_time' => $this->items->sum(fn($item) => $item->default_time_to_deliver)
+        ];
+    }
+}
