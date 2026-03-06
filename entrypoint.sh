16263513cff3859429a8981a7cb11578ac7801bc
milanmarko <marko.milan@students.jedlik.eu>
2026-03-06 12:10:44 +0100

    Remove ulimit setting and simplify entrypoint script by eliminating reverb:start command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
 
-ulimit -n 65535
 php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan storage:link
 
-php artisan reverb:start & php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=8000
