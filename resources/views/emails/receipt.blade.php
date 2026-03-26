28a0e5ece50ba36616b216406cda36bcc69b486e
milanmarko <marko.milan@students.jedlik.eu>
2026-03-26 08:29:50 +0100

    számlában ne a rendeles id-je legyen a titleben hanem valahol a level torzseben Fixes #113

--- resources/views/emails/receipt.blade.php
+++ resources/views/emails/receipt.blade.php
@@ -1,6 +1,7 @@
 <div>
     <h1>Szia {{ $name }}!</h1>
-    <p>Köszönjük a büfés vásárlásodat. A megrendelésed nyugtáját csatolva találod ebben az emailben (PDF
+    <p>Köszönjük a büfés vásárlásodat. A Rendelésed száma: {{ $orderNumber }} A megrendelésed nyugtáját csatolva
+        találod ebben az emailben (PDF
         formátumban).</p>
     <p>Üdvözlettel,<br>JedlikBüfé Csapata</p>
 </div>
