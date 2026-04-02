f8a81017ef42c6fe2cb1bac1db7c950a4b111731
tmarko76 <tmarko76@gmail.com>
2026-04-03 00:39:25 +0200

    Implement logging for channel access checks and create Log model and migration

--- routes/channels.php
+++ routes/channels.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\Log;
 use App\Models\Order;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('ordersOfUser.{userEmail}', function ($user, $userEmail) {
-    error_log("Checking channel access for user: " . $user->email . " against channel email: " . $userEmail);
+    Log::create(['message' => "Checking channel access for user: " . $user->email . " against channel email: " . $userEmail]);
     return ($user->email === $userEmail) || $user->isAdmin();
 });
 Broadcast::channel('orders_admin', function ($user) {
